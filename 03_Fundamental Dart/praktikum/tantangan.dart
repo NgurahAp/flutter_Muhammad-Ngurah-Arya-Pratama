671c2e948ef42072dcd7965d0c6fa3b66445b555
Arya <ngurahpratama2002@gmail.com>
2023-08-18 16:15:54 +0700

    tantangan

---
+++ 03_Fundamental Dart/praktikum/tantangan.dart
@@ -0,0 +1,24 @@
+void main() {
+  // PERSEGI PANJANG
+  int panjangPP = 8;
+  int lebarPP = 6;
+
+  int kelilingPP = panjangPP * 2 + lebarPP * 2;
+
+  print('KELILING PERSEGI PANJANG');
+  print(
+      'Keliling persegi panjang dari panjang  $panjangPP dan lebar sisi $lebarPP adalah $kelilingPP\n');
+
+  //SEGITIGA
+  int panjangSisiA = 20;
+  int panjangSisiB = 10;
+  int panjangSisiC = 30;
+
+  int kelilingS = panjangSisiA + panjangSisiB + panjangSisiC;
+
+  print('\nKELILING SEGITIGA');
+  print('Panjang sisi a = $panjangSisiA ');
+  print('Panjang sisi b = $panjangSisiB ');
+  print('Panjang sisi c = $panjangSisiC ');
+  print('Keliling segitiga dari panjang sisi seperti diatas adalah $kelilingS');
+}
